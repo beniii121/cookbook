ce6f6ee7f1a84a1e5f7633466528d36c0bcc820b
beniii121 <benedek.gal@eldanor.hu>
2024-06-04 10:10:40 +0200

    Cleaning up

--- code/frontend/ui/src/main.jsx
+++ code/frontend/ui/src/main.jsx
@@ -2,18 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter as Router} from 'react-router-dom';
 import Layout from './components/Layout.jsx';
-import {QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 /**
  * Main wrapper for the react app
  */
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <QueryClientProvider client={queryClient}>
-      <Layout />
-    </QueryClientProvider>
+    <Layout />
   </Router>,
 )
